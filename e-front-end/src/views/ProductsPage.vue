075b588f46247199acba560b7ba1e8e4bc2e0a27
Victor <career.victorobije@gmail.com>
2024-05-26 08:34:21 +0100

    FE

---
+++ e-front-end/src/views/ProductsPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="page-wrap">
+    <ProductsGrid :products="products" />
+    <!-- <h1>this is product page</h1> -->
+  </div>
+</template>
+
+<script>
+import { products } from '../fake-data';
+import ProductsGrid from '../components/ProductsGrid.vue';
+
+export default {
+    name: 'ProductsPage',
+    components: {
+      ProductsGrid,
+    },
+    data() {
+      return {
+        products,
+      };
+    }
+};
+</script>
+
